dee2e55f7bbaaebda8b8c0f045d65950d51ccd45
hslavich <hernan.slavich@gmail.com>
2013-09-17 01:34:03 -0300

    Command history in shell

--- src/model/Shell.py
+++ src/model/Shell.py
@@ -2,7 +2,11 @@ from Command import *
 
 class Shell:
     
+    def __init__(self):
+        self.history = []
+    
     def runCommand(self, command):
+        self.history.append(command)
         if hasattr(Command, command):
             function = getattr(Command, command)
             if callable(function):
