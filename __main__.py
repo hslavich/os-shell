780f81fd7d7bc77d02d9dad6d74192c0221e808b
hslavich <hernan.slavich@gmail.com>
2013-11-02 13:50:26 -0300

    Update directory structure

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-from model.shell import Shell
-from gui.shellframe import ShellFrame
+from pyshell.model.shell import Shell
+from pyshell.gui.shellframe import ShellFrame
 
 if __name__ == '__main__':
     shell = Shell()
--- pyshell/model/shell.py
+++ pyshell/model/shell.py
@@ -1,5 +1,3 @@
-from command import *
-
 class Shell:
 
     def __init__(self):
