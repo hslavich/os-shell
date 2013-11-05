b2197b026637fb29f69fde83f4a649d5d7fa41db
hslavich <hernan.slavich@gmail.com>
2013-11-04 22:05:09 -0300

    Support for commands with arguments

--- pyshell/model/shell.py
+++ pyshell/model/shell.py
@@ -7,7 +7,9 @@ class Shell:
 
     def _addExamples(self):
         def _test(): print("Test command")
+        def _echo(args): print(args)
         self.addCommand("test", _test)
+        self.addCommand("echo", _echo)
 
     def addCommand(self, name, command):
         self.commands[name] = command
@@ -15,7 +17,14 @@ class Shell:
     def runCommand(self, command):
         self.history.append(command)
         try:
-            self.commands[command]()
+            comm = self._splitArgs(command)
+            if len(comm) > 1:
+                self.commands[comm[0]](comm[1])
+            else:
+                self.commands[comm[0]]()
             return True
         except (KeyError, TypeError):
             return False
+
+    def _splitArgs(self, command):
+        return command.split(" ", 1)
